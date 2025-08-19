90d18a75ae866566cfae4c9e31a8766b7d467488
poojarivinod <vinod07081@gmail.com>
2025-08-19 12:12:09 +0530

    vpc-module

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" { #terraform aws vpc resource-->Terraform registry
+  cidr_block       = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  instance_tenancy = "default"
+
+  # expense-dev
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.resource_name
+    }
+
+  )
+}
