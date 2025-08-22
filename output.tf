fd774bb3158e4fe8aacaf3e7209a7acd6b2a1266
poojarivinod <vinod07081@gmail.com>
2025-08-22 10:00:55 +0530

    vpc test

--- output.tf
+++ output.tf
@@ -5,3 +5,19 @@
 # output "subnets_info" {
 #     value =  aws_subnet.public
 # }
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id # "*"  means all public id in subnets
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id # "*"  means all public id in subnets
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id # "*"  means all public id in subnets
+}
